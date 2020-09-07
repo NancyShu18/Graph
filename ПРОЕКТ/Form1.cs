4cc50a5069a2b34a8e5c75bfd19e937db41f6db9
Анастасия Шустер <aashuster@edu.hse.ru>
2020-09-07 18:04:32 +0500

    обнова

--- ПРОЕКТ/Form1.cs
+++ ПРОЕКТ/Form1.cs
@@ -13,13 +13,19 @@ namespace ПРОЕКТ
 {
     public partial class Form1 : Form
     {
+        int ilabelX, ilabelY, iMouseX, iMouseY;
+
+
         public Form1()
         {
             InitializeComponent();
+            button1.Enabled = false;
         }
 
         string[] mas = null;
         string[] first_lvl = null;
+        int[] fNs = null;
+        string[] sNt = null;
         string[] second_lvl = null;
         string[] third_lvl = null;
 
@@ -32,24 +38,27 @@ namespace ПРОЕКТ
                     mas = File.ReadAllLines(openFileDialog1.FileName);
                     richTextBox1.Lines = mas;
                     OnScreen();
-                    
+                    button1.Enabled = true;
 
                 }
                 catch (Exception)
-                { }
+                {
+                    MessageBox.Show("Откройте документ согласно синтакису");
+                    richTextBox1.Clear();
+                }
             }
-
         }
 
         void OnScreen()
         {
             bool ok = true;
             string[] splitted = richTextBox1.Text.Split(new char[] { '\n', '-' }, StringSplitOptions.RemoveEmptyEntries);
-            first_lvl = new string[splitted.Length/2];
-            string[]second_lvl_dop = new string[splitted.Length / 2];
-            second_lvl = new string[4];
-            third_lvl = new string[4];
-            for(int x=0,i=0,j=0;x<splitted.Length;x++)
+            foreach (string s in splitted) s.Trim();
+            first_lvl = new string[splitted.Length / 2];
+            fNs = new int[first_lvl.Length];
+            string[] second_lvl_dop = new string[splitted.Length / 2];
+
+            for (int x = 0, i = 0, j = 0; x < splitted.Length; x++)
             {
                 if (ok == true)
                 {
@@ -63,17 +72,31 @@ namespace ПРОЕКТ
                     j++;
                     ok = !ok;
                 }
-                
+            }
+            for (int p = 0; p < second_lvl_dop.Length; p++)
+            {
+                sNt = second_lvl_dop[p].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in sNt)
+                {
+                    second_third.Add(s);
+                }
+            }
+            second_lvl = new string[second_third.Count];
+            third_lvl = new string[second_third.Count];
+            for (int z = 0; z < second_lvl_dop.Length; z++)
+            {
+                string[] s = second_lvl_dop[z].Split(new char[] { '|', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                fNs[z] = s.Length / 2;
             }
             bool ok1 = true;
-            int t = 0;int k = 0;
-            for(int ii=0;ii<second_lvl_dop.Length;ii++)
+            int t = 0; int k = 0;
+            for (int ii = 0; ii < second_lvl_dop.Length; ii++)
             {
-                
                 splitted = second_lvl_dop[ii].Split(new char[] { '|', ';' }, StringSplitOptions.RemoveEmptyEntries);
-                for(int p=0;p<splitted.Length;p++)
+                foreach (string s in splitted) s.Trim();
+                for (int p = 0; p < splitted.Length; p++)
                 {
-                    if(ok1==true)
+                    if (ok1 == true)
                     {
                         second_lvl[t] = splitted[p];
                         t++;
@@ -88,40 +111,119 @@ namespace ПРОЕКТ
                 }
             }
         }
-        
 
-        void DrawLine()
-        {
-
-        }
-        List<Label> lblList = new List<Label>();
+        List<Label> lbl1 = new List<Label>();
+        List<Label> lbl2 = new List<Label>();
+        List<Label> lbl3 = new List<Label>();
+        List<string> second_third = new List<string>();
         void CreateLabel()
         {
-            for(int i=0;i<first_lvl.Length;i++)
+            for (int i = 0; i < first_lvl.Length; i++)
             {
                 Label lbl = new Label();
                 lbl.Text = first_lvl[i];
-                lbl.Location = new Point(300*(i+1), 100);
-                lblList.Add(lbl);
+                lbl.AutoSize = true;
+                lbl.Location = new Point(210 + 100 * (i + 1), 100);
+                lbl.Name = lbl.Text;
+                lbl.AllowDrop = true;
+                lbl.MouseDown += new MouseEventHandler(this.lbl_MouseDown);
+                lbl.MouseMove += new MouseEventHandler(this.lbl_MouseMove);
+                lbl1.Add(lbl);
                 this.Controls.Add(lbl);
 
             }
-            for(int j=0;j<second_lvl.Length;j++)
+            for (int j = 0; j < second_lvl.Length; j++)
             {
                 Label lbl = new Label();
                 lbl.Text = second_lvl[j];
-                lbl.Location = new Point(300+100*(j+1), 120);
-                lblList.Add(lbl);
+                lbl.Location = new Point(210 + 100 * (j + 1), 170);
+                lbl.Name = lbl.Text;
+                lbl.AutoSize = true;
+                lbl.AllowDrop = true;
+                lbl.MouseDown += new MouseEventHandler(this.lbl_MouseDown);
+                lbl.MouseMove += new MouseEventHandler(this.lbl_MouseMove);
+                lbl2.Add(lbl);
                 this.Controls.Add(lbl);
             }
-                   
+            for (int k = 0; k < third_lvl.Length; k++)
+            {
+                bool found = false;
+                Label lbl = new Label();
+                lbl.Text = third_lvl[k];
+                lbl.Name = lbl.Text;
+                foreach (Label labe in lbl3)
+                {
+                    if (labe.Name == lbl.Name)
+                    {
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (!found)
+                {
+                    lbl.Location = new Point(210 + 100 * (k + 1), 240);
+                    lbl.AllowDrop = true;
+                    lbl.AutoSize = true;
+                    lbl.MouseDown += new MouseEventHandler(this.lbl_MouseDown);
+                    lbl.MouseMove += new MouseEventHandler(this.lbl_MouseMove);
+                    lbl3.Add(lbl);
+                    this.Controls.Add(lbl);
+                }
+            }
+            //DrawLine();
+            this.Paint += Form_Paint;
         }
-      
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
             CreateLabel();
         }
-       
+
+        private void lbl_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label label = sender as Label;
+            ilabelX = label.Location.X;
+            ilabelY = label.Location.Y;
+            iMouseX = MousePosition.X;
+            iMouseY = MousePosition.Y;
+        }
+        private void lbl_MouseMove(object sender, MouseEventArgs e)
+        {
+            Label label = sender as Label;
+            int iMouseX2 = MousePosition.X;
+            int iMouseY2 = MousePosition.Y;
+            if (e.Button == MouseButtons.Left)
+                label.Location = new Point(ilabelX + (iMouseX2 - iMouseX), ilabelY + (iMouseY2 - iMouseY));
+        }
+        private void Form_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.Black);
+            int curr = 0;
+            for (int i = 0; i < first_lvl.Length; i++)
+            {
+
+                for (int j = 0; j < fNs[i]; j++)
+                {
+                    e.Graphics.DrawLine(p, lbl1[i].Location, lbl2[curr++].Location);
+                }
+            }
+            for (int i = 0; i < lbl2.Count; i++)
+            {
+                for (int j = 0; j < lbl3.Count; j++)
+                {
+                    for (int k = 0; k < second_third.Count; k++)
+                    {
+                        if (second_third[k].Contains(lbl2[i].Name) && second_third[k].Contains(lbl3[j].Name))
+                        {
+                            e.Graphics.DrawLine(p, lbl2[i].Location, lbl3[j].Location);
+                            break;
+                        }
+
+                    }
+                }
+            }
+            this.Refresh();
+        }
     }
 }
